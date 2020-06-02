7f2063f684faba9134e2e6636e28ddf1ea924e79
Jimil <jimilp7@gmail.com>
2020-06-02 00:55:27 -0400

    Update - 6/2

--- app/src/main/java/com/example/android/namaste_savannah/MainActivity.java
+++ app/src/main/java/com/example/android/namaste_savannah/MainActivity.java
@@ -1,13 +1,89 @@
 package com.example.android.namaste_savannah;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout d1;
+    private ActionBarDrawerToggle abdt;
+
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        d1 = (DrawerLayout) findViewById(R.id.d1);
+        abdt = new ActionBarDrawerToggle(this, d1,  R.string.Open, R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+
+        d1.addDrawerListener(abdt);
+        abdt.syncState();
+
+
+
+        NavigationView nav_view = (NavigationView) findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(this);
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container_fragment, new MainFragment());
+        fragmentTransaction.commit();
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        d1.closeDrawer(GravityCompat.START);
+        if(id == R.id.home)
+        {
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_fragment, new MainFragment());
+            fragmentTransaction.commit();
+        }
+        else if(id == R.id.menu)
+        {
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_fragment, new FragmentMenu());
+            fragmentTransaction.commit();
+        }
+        else if(id == R.id.gallery)
+        {
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_fragment, new FragmentGallery());
+            fragmentTransaction.commit();        }
+        else if(id == R.id.contact)
+        {
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_fragment, new FragmentContact());
+            fragmentTransaction.commit();        }
+
+
+        return true;
     }
 }
